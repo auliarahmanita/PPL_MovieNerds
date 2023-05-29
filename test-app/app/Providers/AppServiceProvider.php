56b5bca2c93c5834a56bfd56eb7d816a2880b35a
safiraanna <annasafira2204@gmail.con>
2023-05-29 18:51:33 +0700

    test admin

--- test-app/app/Providers/AppServiceProvider.php
+++ test-app/app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Gate::define('admin', function (User $user) {
+            return $user['is-admin'];
+        });
     }
 }
--- test-app/resources/views/login/index.blade.php
+++ test-app/resources/views/login/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <div class="row justify-content-center">
         <div class="col-md-5" >
             @if (session()->has('success'))
