204e8623978e60b660761f21cb4ab3f151b709a5
auliarahmanita <auliarahmanita07@gmail.com>
2023-06-05 01:19:55 +0700

    review article

--- test-app/resources/views/dashboard/admin/review-article.blade.php
+++ test-app/resources/views/dashboard/admin/review-article.blade.php
@@ -1,26 +1,49 @@
-@extends('layouts.main') 
+@extends('layouts.main')
+
+@section('head')
+    <!-- <link rel="stylesheet" href="{{ asset('css/tierlist.css') }}">   -->
+    <!-- <link rel="stylesheet" href="{{ asset('css/article.css') }}"> -->
+@endsection
 
 @section('container') 
+<link rel="stylesheet" href="{{ asset('css/tierlist.css') }}">  
+<section class="peringkat">
+        <div class="my-width">
+        <div class="title-section"><p>Review Article</p></div>
+        <div class="garis-section"></div>
+    <div class="article">
+    <div class="data-login">
+    <label>Title:</label>
+    <h1>{{ $article->title }}</h1>
+    </div>
 
-    <h1>Review Article</h1>
-
-    <h2>Title: {{ $article->title }}</h2>
-    <p>Author: {{ $article->author->username }}</p>
-    <p>Content: {!! $article['konten'] !!}</p>
+    <div class="data-login">
+    <label>Author:</label>
+    <h1>{{ $article->author->username }}</h1>
+    </div>
+    
+    <div class="data-login">
+    <label>Content:</label>
+    <h2>{!! $article['konten'] !!}</h2>
+    </div>
 
     <form action="{{ route('admin.review.update', $article->id) }}" method="POST">
         @method('PATCH')
         @csrf
-        <div>
+        <div class="data-login">
             <label for="reviewed">Review Status:</label>
             <select name="reviewed" id="reviewed">
                 <option value="1">Disetujui</option>
                 <option value="-1">Ditolak</option>
             </select>
         </div>
-        <div>
+        <div class="container-bawah">
+            <div class="btn-kirim">
             <button type="submit">Kirim</button>
+            </div>
         </div>
     </form>
-
+    </div>
+</div>
+</section>
 @endsection
--- test-app/resources/views/dashboard/admin/review-list.blade.php
+++ test-app/resources/views/dashboard/admin/review-list.blade.php
@@ -1,14 +1,25 @@
-@extends('layouts.main') 
+@extends('layouts.main')
+
+@section('head')
+    <link rel="stylesheet" href="{{ asset('css/tierlist.css') }}">  
+    <!-- <link rel="stylesheet" href="{{ asset('css/article.css') }}"> -->
+@endsection
 
 @section('container') 
 
-    <h1>Review List</h1>
+<section class="peringkat">
+        <div class="my-width">
+        <div class="title-section"><p>Review List</p></div>
+        <div class="garis-section"></div>
 
+
+        <div class="karesel">
+        <div class="table-wrapper">
     @if (count($articles) > 0)
-        <table>
+        <table class="fl-table">
             <thead>
                 <tr>
-                    <th>Title</th>
+                    <th style="text-align: left">Title</th>
                     <th>Author</th>
                     <th>Actions</th>
                 </tr>
@@ -16,10 +27,15 @@
             <tbody>
                 @foreach ($articles as $article)
                     <tr>
-                        <td>{{ $article->title }}</td>
+                        <td style="text-align: left;">{{ $article->title }}</td>
                         <td>{{ $article->author->username }}</td>
                         <td>
-                            <a href="{{ route('admin.review.article', $article->id) }}">Review</a>
+                            <!-- <a href="{{ route('admin.review.article', $article->id) }}">Review</a> -->
+                            <!-- <div style="text-align: right;text-align: right; margin-top: 13%; margin-bottom: 40px;"> -->
+                            <a href="{{ route('admin.review.article', $article->id) }}" style="padding: 5px;background-color:#FFD233;color:black;border-radius:5px;">
+                            Review
+                        </a>
+                    <!-- </div> -->
                         </td>
                     </tr>
                 @endforeach
@@ -28,5 +44,8 @@
     @else
         <p>No posts to review.</p>
     @endif
-
+    </div>
+    </div>
+</div>
+</section>
 @endsection
